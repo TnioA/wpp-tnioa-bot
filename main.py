0ea8262106c53a559eeb0bf03d90a240bad27964
Tanio <hortanio@hotmail.com>
2019-03-14 21:23:27 -0300

    .

--- main.py
+++ main.py
@@ -1,15 +1,62 @@
 # -*- coding: utf-8 -*-
-from app import Botzap
+import json, requests
+from simpleapp import Botzap
 import re
 
+def getnews():
+	#API DE NOTICIAS BRASIL
+	url = 'https://newsapi.org/v2/top-headlines?sources=globo&pageSize=5&apiKey=f6fdb7cb0f2a497d92dbe719a29b197f'
+	#http = urllib3.PoolManager()
+	#resp = http.request('GET',url)
+	resp = requests.get(url)
+	conteudo = json.loads(resp.content)
+	return(conteudo)
 
-contato = 'Samuel'
-contato = str(contato)
-mensagem = 'Bot:: Mensagem de teste enviada pelo bot do Tanio'
-mensagem = str(mensagem)
+
+def getmovies():
+	#API DE FILMES BRASIL
+	url = 'http://localhost:5000/api/filmes'
+	resp = requests.get(url)
+	conteudo = json.loads(resp.content)
+	return(conteudo)
 
 
-bot = Botzap('robozin')
+contato = str('BotTnioA')
+mensagem = str('Bot:: Mensagem de teste enviada pelo bot do Tanio')
 
-bot.send(contato, mensagem)
-#bot.read()
+
+bot = Botzap('Teste')
+
+#bot.send(contato, mensagem)
+
+while True:
+	retorno = bot.monitor()
+
+	if retorno == 'Bot:oibot':
+		print('recebido comando ola')
+		bot.write('Oi eu sou o bot do Tanio responda com Bot:comandos para receber a lista de comandos e conseguir se comunicar comigo')
+	
+	if retorno == 'Bot:filmes':
+		conteudo = getmovies()
+		c = 1
+		print('recebido o comando filmes')
+		for cont in conteudo['filmes']:
+			bot.write('Filme {}: \n'.format(c) + 'Titulo: ' + cont['nome'] + ' Estreado em: ' + cont['data'])
+			c = int(c) + 1
+	
+	if retorno == 'Bot:futebol':
+		print('recebido o comando futebol')
+		bot.write('Ainda em desenvolvimento')
+	
+	if retorno == 'Bot:noticias':
+		print('recebido o comando noticias')
+		conteudo = getnews()
+		c = 1
+		for cont in conteudo['articles']:
+			bot.write('Noticia {}: '.format(c) + cont['title'])
+			c = int(c) + 1
+
+	if retorno == 'Bot:comandos':
+		print('recebido comando lista de comandos')
+		bot.write('Comandos ==================================> \n Bot:oibot = Para me chamar \n Bot:noticias = Para receber algumas noticias \n Bot:futebol = Para receber a tabela do Brasileirao Serie A \n Bot:filmes = Para receber os filmes que estao em cartaz \n Bot:comandos = Para receber uma lista de comandos \n >>=============================>')
+pass
--- simpleapp.py
+++ simpleapp.py
@@ -13,22 +13,27 @@ class Botzap:
 	def __init__(self, nome):
 		
 		print('Bot Iniciado')
+		teste = ('''Esta e a string
+E aqui vai a outra parte dela''')
 		self.driver = webdriver.Chrome(executable_path='driver/chromedriver')
 		self.driver.get('https://web.whatsapp.com')
 		time.sleep(5)
 		#driver = webdriver.Edge(executable_path='\..') - se for microsoft edge
 		#driver = webdriver.Firefox(executable_path='\..') se for firefox
-
-
-	def send(self, nome_contato, texto):
-
 		pesquisaBox = self.driver.find_element_by_class_name('jN-F5')
-		pesquisaBox.send_keys(nome_contato)
+		pesquisaBox.send_keys(nome)
 		time.sleep(2)
-
-		contato = self.driver.find_element_by_xpath('//span[@title = "{}"]'.format(nome_contato))
+		contato = self.driver.find_element_by_xpath('//span[@title = "{}"]'.format(nome))
 		contato.click()
 		time.sleep(1)
+		mensagemBox = self.driver.find_element_by_class_name('_2S1VP')
+		mensagemBox.send_keys(teste)
+		time.sleep(1)
+		enviar = self.driver.find_element_by_class_name('_35EW6')
+		enviar.click()
+
+	def write(self, texto):
+		#print('Comando enviar mensagem')
 
 		mensagemBox = self.driver.find_element_by_class_name('_2S1VP')
 		mensagemBox.send_keys(texto)
@@ -37,20 +42,17 @@ class Botzap:
 		enviar = self.driver.find_element_by_class_name('_35EW6')
 		enviar.click()
 
-		limpar_pesquisa = self.driver.find_element_by_class_name('C28xL')
-		limpar_pesquisa.click()
-		limpar_pesquisa.click()
+	def monitor(self):
+		#print('Comando escutar iniciado')
+		#print('Buscar ultima conversa')
+		post = self.driver.find_elements_by_class_name('_3_7SH')
+		ultimo = len(post) - 1
+		try:
+			texto = post[ultimo].find_element_by_class_name('selectable-text').text
+			if texto[:4] == 'Bot:':
+				return(texto)
+		except:
+			pass
 
 
-	def read(self):
-		for conversa in self.driver.find_elements_by_class_name('_3j7s9'):
-			conversa.click()
-			time.sleep(1)
-			post = self.driver.find_elements_by_class_name('_3_7SH')
-			ultimo = len(post) - 1
-			try:
-				texto = post[ultimo].find_element_by_class_name('selectable-text').text.strip()
-				if texto[:4] == 'Bot:':
-					return(texto)
-			except:
-				pass
+
